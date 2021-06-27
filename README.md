ea5a397c9aaa764ba6b59f26346aaeec54703350
Matthias Berse <matthias.berse@gmail.com>
2021-06-27 22:07:34 +0200

    Refactored secrets out of main source code

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # esp8266-keypad-mqtt
 Connect a Keypad to an MQTT server with an ESP8266
 
-I used a Wingoneer keypad that came in a pack of 5 and just wired from left to right to D1 - D7 and the last pin to D0 on a WeMos D1 Mini.
-
-I don't really plan on doing much more with it and don't plan on offering support if you can't get it to work, as I'm definitely a n00b when it comes to this stuff.
+By default it will send whatever number you punch in to /keypad/KEY on your MQTT broker when you press * on the keypad.
 
 # Setup
 
@@ -27,8 +25,10 @@ pio run --target upload
 
 # Usage
 
-By default it will send whatever number you punch in to /keypad/KEY on your MQTT broker when you press * on the keypad.
+Restructured the code from the fork and did some clean up. 
 
-I tried to make everything easy to customize but got a little stuck on making the topic easy to customize so if you want to fix that and submit a pull that'd be great.
+Details for your setup now go into ``secrets.h`` You will need to copy the ``secrets.h.dist`` to ``secrets.h`` and fill in your details.
 
-Enjoy.
+Also added the ability to connect to a FritzBox and ring connected phones.
+
+More documentation to follow.
--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,33 @@
 #include <ESP8266WiFi.h>        
 #include <PubSubClient.h>
 #include <Keypad.h>
+#include <tr064.h>
+#include "secrets.h"
 
 //WiFi info
-const char* ssid     = "tranquility";         // SSID
-const char* password = "gabbahey!holetsgo";     // Passphrase
+const char* ssid     = SECRET_WIFISSID;      //defined in secrets.h 
+const char* password = SECRET_WIFIPASS;     
+const char* deviceName = SECRET_DEVICENAME;
 
 //MQTT info
-const char* mqttServer = "smartberry"; //MQTT Server
-const int mqttPort = 1883; //MQTT Port
-const char* mqttUser = ""; //MQTT User
-const char* mqttPassword = ""; //MQTT Password
+const char* mqttServer = SECRET_MQTTSERVER; //MQTT Server
+const int mqttPort = SECRET_MQTTPORT; //MQTT Port
+const char* mqttUser = SECRET_MQTTUSER; //MQTT User
+const char* mqttPassword = SECRET_MQTTPASS; //MQTT Password
 
 //Keypad info
 const char sendKey = '*'; //Keypress to initialize send to MQTT
 const byte n_rows = 4;
 const byte n_cols = 4;
+
+//Fritzbox info
+const char FB_USER[] = SECRET_FBUSER;
+const char FB_PASSWORD[] = SECRET_FBPASS; 
+const char FRITZBOX_IP[] = SECRET_FBIP;
+const int FRITZBOX_PORT = 49000;
+const char ringKey = 'A'; // Keypress to ring the doorbell
+
+TR064 tr064_connection(FRITZBOX_PORT, FRITZBOX_IP, FB_USER, FB_PASSWORD);
 
 char keys[n_rows][n_cols] = { //Define values of keys
   {'1','2','3','A'},
@@ -29,32 +41,32 @@ byte colPins[n_rows] = {D1, D2, D3, D4}; //Define row pins
 byte rowPins[n_cols] = {D0, D5, D6, D7}; //Define column pins
 
 Keypad Keypad1 = Keypad( makeKeymap(keys), rowPins, colPins, n_rows, n_cols);
-String fullKey;
+String fullKey = String("");
 char KeyMQTT[50];
 WiFiClient espClient;
 PubSubClient mqttclient(espClient);
 
-  void wificn () {               //Connect to WiFi
-    if(WiFi.status() != WL_CONNECTED) {
+void wificn () {               //Connect to WiFi
+  WiFi.hostname(deviceName);
+  if (WiFi.status() != WL_CONNECTED){
      WiFi.begin(ssid, password);             
      Serial.print("Connecting to ");
      Serial.print(ssid); Serial.println(" ...");
 
      int i = 0;
-     while (WiFi.status() != WL_CONNECTED) { 
-     delay(1000);
-     Serial.print(++i); Serial.print(' ');
-      }
+     while (WiFi.status() != WL_CONNECTED){ 
+        delay(200);
+        Serial.print(++i); Serial.print(' ');
+     }
      Serial.println('\n');
      Serial.println("Connection established!");  
      Serial.print("IP address:\t");
      Serial.println(WiFi.localIP());        
-   
-  }    
- }
+    }    
+}
 
 void mqttcn () {              //Connect to MQTT
-        mqttclient.setServer(mqttServer, mqttPort);
+   mqttclient.setServer(mqttServer, mqttPort);
    
     while (!mqttclient.connected()) {
     Serial.println("Connecting to MQTT...");
@@ -83,15 +95,41 @@ void setup() {
   
 }
 
+void fb_ring(){
+//Ring all phones connected to FB to simulate a doorbell
+   tr064_connection.init();
+
+   String tr064_service = "urn:dslforum-org:service:X_VoIP:1";
+
+   // **9 is Fritzbox-Round Call number.
+   String call_params[][2] = {{"NewX_AVM-DE_PhoneNumber", "**9"}};
+   String req[][2] ={{}};
+   // tr064_connection.debug_level = 4;
+   tr064_connection.action(tr064_service, "X_AVM-DE_DialNumber", call_params, 1, req, 0);
+   // Warte vier Sekunden bis zum auflegen
+   delay(6000);
+   tr064_connection.action(tr064_service, "X_AVM-DE_DialHangup");
+   
+}
+
 void loop() {
     char Key1 = Keypad1.getKey();
+    if(Key1 == ringKey){
+       Serial.print(Key1);
+       Serial.print("Ringing the bell");
+       fullKey = "";
+       Key1 = char('\0'); //Don't want to add the ring key to a sequence
+       mqttclient.publish("keypad/RING","1");
+       fb_ring();
+
+    }
     if(Key1 == sendKey) {
      fullKey.toCharArray(KeyMQTT,fullKey.length()+1);
      mqttclient.publish("keypad/KEY", KeyMQTT);
      Serial.print("Sent key: ");
      Serial.println(fullKey);
      fullKey = "";
-    } else if (Key1 != NULL){
+    } else if (Key1 != '\0'){
      fullKey += Key1;
      Serial.print("Current keys: ");
      Serial.println(fullKey);
